8db73757143575ab4f85093aba744afbdf436d3a
김민석 <61860897+kju1018@users.noreply.github.com>
2026-03-08 20:53:27 +0900

    fix: 배포 에러 수정

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/I_SEOUL_U_3-1/",
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
@@ -13,8 +14,8 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
     css: true,
   },
-} as any)
+} as any);
